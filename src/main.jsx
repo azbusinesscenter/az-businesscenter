1ad0681a12050b053af24ac684023e22a6758d70
azbusinesscenter <az.businesscenter10@gmail.com>
2026-05-30 22:57:05 +0200

    Ajout du retour en haut de page au changement de page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import App from "./App";
 import Activities from "./Activities";
 
 import "./index.css";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function Root() {
   const [loading, setLoading] = useState(true);
@@ -48,6 +56,7 @@ function Root() {
 
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/activities" element={<Activities />} />
